145173a3b12ff4e342eb10c4862583f5f61d3ef3
ieatcrayolaaa <dsgbdomingo@gmail.com>
2019-09-04 12:58:49 +0800

    Added smooth scrolling

--- js/grayscale.js
+++ js/grayscale.js
@@ -40,28 +40,6 @@ $(document).ready(function(){
     });
 });
 
-// jQuery to collapse the navbar on scroll
-$(document).ready(function () {
-    
-    'use strict';
-
-    var c, currentScrollTop = 0,
-    navbar = $('nav');
-
-    $(window).scroll(function () {
-        var a = $(window).scrollTop();
-        var b = navbar.height();
-
-        currentScrollTop = a;
-
-        if (c < currentScrollTop && a > b + b) {
-            navbar.addClass("scrollUp");
-        } else if (c > currentScrollTop && !(a <= b)) {
-            navbar.removeClass("scrollUp");
-        }
-        c = currentScrollTop;
-    });
-});
 
 
 
@@ -99,7 +77,7 @@ $(document).ready(function(){
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.about-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
@@ -261,6 +239,64 @@ function init() {
 }
 });
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+//smooth scrolling
 
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
+// jQuery to collapse the navbar on scroll
+$(document).ready(function () {
+    
+    'use strict';
+
+    var c, currentScrollTop = 0,
+    navbar = $('nav');
+
+    $(window).scroll(function () {
+        var a = $(window).scrollTop();
+        var b = navbar.height();
+
+        currentScrollTop = a;
+
+        if (c < currentScrollTop && a > b + b) {
+            navbar.addClass("scrollUp");
+        } else if (c > currentScrollTop && !(a <= b)) {
+            navbar.removeClass("scrollUp");
+        }
+        c = currentScrollTop;
+    });
+});
